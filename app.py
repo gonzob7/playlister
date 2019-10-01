50200642b70485641cfe46b6c51d76719d2fb021
Gonzalo Birrueta <silentcr7@gmail.com>
2019-10-01 14:24:53 -0700

    display playlist on home page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -14,7 +15,7 @@ app = Flask(__name__)
 #]
 
 @app.route('/')
-def playlist_index():
+def playlists_index():
     '''Show all playlists.'''
     return render_template('playlists_index.html', playlists=playlists.find())
 
@@ -22,6 +23,18 @@ def playlist_index():
 def playlists_new():
     '''Create a new playlist'''
     return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description')
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
